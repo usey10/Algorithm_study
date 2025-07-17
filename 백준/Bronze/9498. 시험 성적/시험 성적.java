88362f63c356dd1cf8559f85f933a34a893bc201
김요은 <tchy1534@gmail.com>
2025-07-17 09:28:02 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String temp = br.readLine();
+		int score = Integer.parseInt(temp);
+		String grade;
+		if (score >= 90) {
+			grade = "A";
+		} else if (score >= 80) {
+			grade = "B";
+		} else if (score >= 70) {
+			grade = "C";
+		} else if (score >= 60) {
+			grade = "D";
+		} else {
+			grade = "F";
+		}
+		System.out.println(grade);
+	}
+}
