65572ec6dc9131718c4a232b04bc643f222b40c9
김요은 <tchy1534@gmail.com>
2025-08-16 14:52:49 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] dps = new int[n+1];
+		dps[1] = 1;
+		
+		if (n >= 2) {
+			dps[2] = 2;
+			
+			for (int i=3;i<=n;i++) {
+				dps[i] = (dps[i-1] + dps[i-2])%10007;
+			}
+			
+		}
+		System.out.println(dps[n]);
+		
+	}
+}
